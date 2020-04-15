7f1bc6f609d08b3bd99fcdc5680f0d2128ac0f1f
decent-scraper-1450 <jaimealba1574@gmail.com>
2020-04-15 21:15:00 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,16 +1,9 @@
 require 'pry'
 
-def my_all?(collection)
+def my_find(collection)
   i = 0
-  block_return_values = []
   while i < collection.length
-    block_return_values << yield(collection[i])
+    return collection[i] if yield(collection[i])
     i = i + 1
-  end
-  
-  if block_return_values.include?(false)
-    false
-  else
-    true
   end
 end
